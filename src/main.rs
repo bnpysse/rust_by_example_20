fed60a6cb94bd00480bbe1241d5cedf684be6eee
bnpysse <bnpysse@aliyun.com>
2023-02-05 20:46:28 +0800

    Completed File I/O.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::fs::File;
+use std::fs::{File, OpenOptions};
+use std::io;
 use std::io::Write;
 
 // 20.3.路径
@@ -83,9 +84,111 @@ fn main() {
         Err(why) => panic!("couldn't write to {}: {:?}", display, why),
         Ok(_) => println!("successfully wrote {}", display),
     }
+    //endregion
 
+    //region 20.4.3.读取行
+    println!("\n\n=====20.4.3.读取行=====");
+    // 方法 lines() 在文件的行上返回一个迭代器
+    // File::open 需要一个泛型 AsRef<Path>，这正是 read_lines() 期望的输入。
+    use std::io::{self, BufRead};
+    if let Ok(lines) = read_lines("src/main.rs") {
+        for line in lines {
+            if let Ok(content) = line {
+                println!("{}", content);
+            }
+        }
+    }
 
+    fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+        where P: AsRef<Path>, {
+        let file = File::open(filename)?;
+        Ok(io::BufReader::new(file).lines())
+    }
+    //endregion
 
+    //region 20.6.文件系统操作
+    println!("\n\n=====20.6.文件系统操作=====");
+    use std::fs;
+    use std::fs::OpenOptions;
+    use std::os::unix;
+    use std::io::prelude::*;
+    // `% cat path` 的简单实现
+    fn cat(path: &Path) -> io::Result<String> {
+        let mut f = File::open(path)?;
+        let mut s = String::new();
+        match f.read_to_string(&mut s) {
+            Ok(_) => Ok(s),
+            Err(e) => Err(e),
+        }
+    }
+    // `% echo s > path`的简单实现
+    fn echo(s: &str, path: &Path) -> io::Result<()> {
+        let mut f = File::create(path)?;
+        f.write_all(s.as_bytes())
+    }
+    // `% touch path`
+    fn touch(path: &Path) -> io::Result<()> {
+        match OpenOptions::new().create(true).write(true).open(path) {
+            Ok(_) => Ok(()),
+            Err(e) => Err(e),
+        }
+    }
+
+    println!("mkdir a");
+    match fs::create_dir("a") {
+        Err(why) => println!("! {:?}", why.kind()),
+        Ok(_) => {},
+    }
+    println!("`echo hello > a/b.txt`") ;
+    echo("hello", &Path::new("a/b.txt")).unwrap_or_else(|why| {
+        println!("! {:?}", why.kind());
+    });
+
+    println!("`mkdir -p a/c/d`");
+   // 递归地创建一个目录，返回 `io::Result<()>`
+    fs::create_dir_all("a/c/d").unwrap_or_else(|why| {
+        println!("! {:?}", why.kind());
+    });
+
+    println!("`touch a/c/e.txt`");
+    touch(&Path::new("a/c/e.txt")).unwrap_or_else(|why| {
+        println!("! {:?}", why.kind());
+    });
+
+    println!("`ln -s ../b.txt a/c/b.txt`");
+    // 创建一个符号链接，返回 `io::Result<()>`
+    if cfg!(target_family = "unix") {
+        unix::fs::symlink("../b.txt", "a/c/b.txt").unwrap_or_else(|why| {
+        println!("! {:?}", why.kind());
+        });
+    }
+
+    println!("`cat a/c/b.txt`");
+    match cat(&Path::new("a/c/b.txt")) {
+        Err(why) => println!("! {:?}", why.kind()),
+        Ok(s) => println!("> {}", s),
+    }
+
+    println!("`ls a`");
+    // 读取目录的内容，返回 `io::Result<Vec<Path>>`
+    match fs::read_dir("a") {
+        Err(why) => println!("! {:?}", why.kind()),
+        Ok(paths) => for path in paths {
+            println!("> {:?}", path.unwrap().path());
+        },
+    }
+
+    println!("`rm a/c/e.txt`");
+    // 删除一个文件，返回 `io::Result<()>`
+    fs::remove_file("a/c/e.txt").unwrap_or_else(|why| {
+        println!("! {:?}", why.kind());
+    });
+
+    println!("`rmdir a/c/d`");
+    // 移除一个空目录，返回 `io::Result<()>`
+    fs::remove_dir("a/c/d").unwrap_or_else(|why| {
+        println!("! {:?}", why.kind());
+    });
 
 
 
